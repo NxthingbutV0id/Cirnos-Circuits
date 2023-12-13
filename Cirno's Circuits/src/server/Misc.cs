5358ae07b992adafb7958c324fc27447e127056c
NxthingbutV0id <mannyt5889@gmail.com>
2023-12-13 14:07:24 -0500

    Bugfixes

--- Cirno's Circuits/src/server/Misc.cs
+++ Cirno's Circuits/src/server/Misc.cs
@@ -55,42 +55,16 @@ namespace CirnosCircuits {
 	public class Comparator : LogicComponent {
 		protected override void DoLogicUpdate() {
 			var utils = new Utils(Inputs, Outputs);
-			dynamic a = 0, b = 0;
+			dynamic a, b;
 			bool signed = Inputs[Inputs.Count - 1].On;
 			int bitres = (Inputs.Count - 1) >> 1;
-			switch (bitres, signed) {
-				case (8, true):
-					a = utils.InputValue<sbyte>(0, bitres);
-					b = utils.InputValue<sbyte>(bitres, bitres << 1);
-					break;
-				case (8, false):
-					a = utils.InputValue<byte>(0, bitres);
-					b = utils.InputValue<byte>(bitres, bitres << 1);
-					break;
-				case (16, true):
-					a = utils.InputValue<short>(0, bitres);
-					b = utils.InputValue<short>(bitres, bitres << 1);
-					break;
-				case (16, false):
-					a = utils.InputValue<ushort>(0, bitres);
-					b = utils.InputValue<ushort>(bitres, bitres << 1);
-					break;
-				case (32, true):
-					a = utils.InputValue<int>(0, bitres);
-					b = utils.InputValue<int>(bitres, bitres << 1);
-					break;
-				case (32, false):
-					a = utils.InputValue<uint>(0, bitres);
-					b = utils.InputValue<uint>(bitres, bitres << 1);
-					break;
-				case (64, true):
-					a = utils.InputValue<long>(0, bitres);
-					b = utils.InputValue<long>(bitres, bitres << 1);
-					break;
-				case (64, false):
-					a = utils.InputValue<ulong>(0, bitres);
-					b = utils.InputValue<ulong>(bitres, bitres << 1);
-					break;
+
+			if (signed) {
+				a = utils.InputValue<long>(0, bitres);
+				b = utils.InputValue<long>(bitres, bitres << 1);
+			} else {
+				a = utils.InputValue<ulong>(0, bitres);
+				b = utils.InputValue<ulong>(bitres, bitres << 1);
 			}
 
 			Outputs[0].On = a > b;
