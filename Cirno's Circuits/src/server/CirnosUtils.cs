7e4d68a5467a0b6cf99aa647ddd4d4a3f80a910b
NxthingbutV0id <mannyt5889@gmail.com>
2023-12-13 16:23:49 -0500

    Fixes

--- Cirno's Circuits/src/server/CirnosUtils.cs
+++ Cirno's Circuits/src/server/CirnosUtils.cs
@@ -29,7 +29,7 @@ namespace CirnosCircuits {
 		public ulong GrabValueFromInput(int offset = 0) {
 			ulong result = 0;
 
-			for (int i = offset; i < inputs.Count + offset; i++) {
+			for (int i = offset; i < inputs.Count; i++) {
 				if (inputs[i].On) {
 					result |= 1ul << i;
 				}
@@ -60,49 +60,49 @@ namespace CirnosCircuits {
 
 		//OutputInteger(number, offset = 0)
 		public void OutputInteger(ulong number, int offset = 0) {
-			for (int i = offset; i < (sizeof(ulong) * 8) + offset; i++) {
+			for (int i = offset; i < (sizeof(ulong) * 8); i++) {
 				outputs[i].On = ((number >> (i - offset)) & 1) == 1;
 			}
 		}
 
         public void OutputInteger(long number, int offset = 0) {
-            for (int i = offset; i < (sizeof(long) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(long) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
 
         public void OutputInteger(uint number, int offset = 0) {
-            for (int i = offset; i < (sizeof(uint) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(uint) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
 
         public void OutputInteger(int number, int offset = 0) {
-            for (int i = offset; i < (sizeof(int) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(int) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
 
         public void OutputInteger(ushort number, int offset = 0) {
-            for (int i = offset; i < (sizeof(ushort) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(ushort) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
 
         public void OutputInteger(short number, int offset = 0) {
-            for (int i = offset; i < (sizeof(short) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(short) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
 
         public void OutputInteger(byte number, int offset = 0) {
-            for (int i = offset; i < (sizeof(byte) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(byte) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
 
         public void OutputInteger(sbyte number, int offset = 0) {
-            for (int i = offset; i < (sizeof(sbyte) * 8) + offset; i++) {
+            for (int i = offset; i < (sizeof(sbyte) * 8); i++) {
                 outputs[i].On = ((number >> i) & 1) == 1;
             }
         }
